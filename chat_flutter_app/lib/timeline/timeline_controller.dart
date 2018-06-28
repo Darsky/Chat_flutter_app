f125cd99da21725ebf9dead9d744a22fa2f107c2
Darsky <276294393@qq.com>
2018-06-28 18:17:34 +0800

    增加tabbar

---
+++ chat_flutter_app/lib/timeline/timeline_controller.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TimelineController extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Fetch Data Example',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Fetch Data Example'),
+        ),
+        body: new Center(
+          child: new Text('Timeline'),
+        ),
+      ),
+    );
+  }
+}
